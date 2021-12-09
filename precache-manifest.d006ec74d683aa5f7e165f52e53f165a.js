cf987b5dc62661e561a4cc0b08e5399317f262c9
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2021-12-09 15:45:33 +0530

    Add files via upload

---
+++ precache-manifest.d006ec74d683aa5f7e165f52e53f165a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9128e77d24cd3b0338ffc6e3a70cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae452e026b7b2449cca3",
+    "url": "/static/css/main.a4135bf3.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae452e026b7b2449cca3",
+    "url": "/static/js/main.bc7f9496.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
